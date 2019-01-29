2dd1bea3c887e7679ec2c0e5fbeb47d0bffabc19
Sean McPherson <seanmcp25@gmail.com>
2019-01-29 06:33:16 -0500

    Spin out player results component

--- src/components/views/PlayView/index.js
+++ src/components/views/PlayView/index.js
@@ -2,13 +2,8 @@ import React from 'react'
 import { Redirect } from '@reach/router'
 
 import BigFunLink from 'components/common/BigFunLink'
-import Input from 'components/common/Input'
-import {
-  StyledDivPlayer,
-  StyledDivPlayerHead,
-  StyledDivRounds,
-  StyledH2
-} from './styled'
+import PlayerResults from 'components/common/PlayerResults'
+import { StyledH2 } from './styled'
 
 function PlayView({
   location: { hash },
@@ -26,45 +21,10 @@ function PlayView({
       props.navigate('/play')
     }
   })
-  function renderPlayers() {
-    const output = []
-    for (const id in players) {
-      const player = players[id]
-      output.push(
-        <StyledDivPlayer key={id}>
-          <StyledDivPlayerHead>
-            <span aria-label="Player name">{player.name}</span>
-            <span aria-label="Total score">
-              {calculateTotal(player.results)} points
-            </span>
-          </StyledDivPlayerHead>
-          <StyledDivRounds>
-            {['1', '2', '3'].map(round => (
-              <Input
-                ariaLabel={`Round ${round} score for ${player.name}`}
-                key={`${id}-${round}`}
-                onChange={e => recordResult(id, round, e.target.value)}
-                type="number"
-                value={player.results[round]}
-              />
-            ))}
-          </StyledDivRounds>
-        </StyledDivPlayer>
-      )
-    }
-    return output
-  }
-  function calculateTotal(results) {
-    let total = 0
-    for (const round in results) {
-      total += Number(results[round])
-    }
-    return total
-  }
   return (
     <React.Fragment>
       <StyledH2>Play!</StyledH2>
-      {renderPlayers()}
+      <PlayerResults players={players} recordResult={recordResult} />
       <BigFunLink to="/pudding" translation="Next!">
         次!
       </BigFunLink>
--- src/components/views/PlayView/styled.js
+++ src/components/views/PlayView/styled.js
@@ -3,26 +3,3 @@ import styled from 'styled-components'
 export const StyledH2 = styled.h2`
   margin-top: 0;
 `
-
-export const StyledDivPlayer = styled.div`
-  display: grid;
-  grid-gap: 0.5rem;
-  margin-top: 1rem;
-`
-
-export const StyledDivPlayerHead = styled.div`
-  display: flex;
-  font-weight: bold;
-  justify-content: space-between;
-`
-
-export const StyledDivRounds = styled.div`
-  display: grid;
-  grid-gap: 0.5rem;
-  grid-template-columns: repeat(3, 1fr);
-
-  input {
-    padding: 0.5rem 0;
-    width: 100%;
-  }
-`
