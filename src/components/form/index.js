844c6bdd84600e6b74153913283fa7358e3aad20
Sean McPherson <mail@seanmcp.com>
2019-05-25 10:24:49 -0400

    Create shared form elements

---
+++ src/components/form/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+export const Label = styled.label`
+  display: block;
+  font-weight: bold;
+  margin: 0.5rem 0;
+`
+
+export const Description = styled.p`
+  font-style: italic;
+  font-size: 0.875rem;
+  margin: 0.5rem 0;
+`
+
+export const Input = styled.input`
+  background: white;
+  border: 0;
+  border-radius: 0.5rem;
+  font-family: inherit;
+  font-size: inherit;
+  outline: 0;
+  padding: 1rem;
+  text-align: center;
+  transition: border 200ms ease-in-out;
+
+  ${props =>
+    props.maxWidth &&
+    css`
+      max-width: ${props.maxWidth};
+    `}
+
+  &:focus {
+    box-shadow: 0 0 0 4px ${props => props.theme.ORANGE};
+  }
+`
+
+export const UncontrolledInput = React.forwardRef((props, ref) => (
+  <Input {...props} ref={ref} />
+))
